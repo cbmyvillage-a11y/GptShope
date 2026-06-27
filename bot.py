49332b2cb897409196592274cc5553b99a156c48
cbmyvillage-a11y <cbmyvillage@gmail.com>
2026-06-27 17:55:50 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-print("Bot Ready")
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "স্বাগতম! 🤖 এটি আমার Telegram Bot।")
+
+bot.infinity_polling()
